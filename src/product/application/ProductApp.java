e7c998670e0c0ce5bb9836c6dd4c25b4d6ae5f30
dyaksa <diasnour0395@gmail.com>
2024-04-16 10:11:36 +0700

    equals and hashcode

--- src/product/application/ProductApp.java
+++ src/product/application/ProductApp.java
@@ -9,5 +9,9 @@ public class ProductApp {
         System.out.println(product.price);
 
         System.out.println(product);
+
+        Product product2 = new Product("Coca cola", 2000);
+        System.out.println(product.equals(product2));
+        System.out.println(product.hashCode() == product2.hashCode());
     }
 }
--- src/product/data/Product.java
+++ src/product/data/Product.java
@@ -1,5 +1,7 @@
 package product.data;
 
+import java.util.Objects;
+
 public class Product {
     public String name;
     public int price;
@@ -12,4 +14,28 @@ public class Product {
     public String toString() {
         return "Product " + name + " with price " + price;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    //    public boolean equals(Object o){
+//        if(this == o) return true;
+//
+//        if(!(o instanceof Product)) return false;
+//
+//        Product p = (Product) o;
+//
+//        if(this.price != p.price) return false;
+//        return name != null ? name.equals(p.name) : p.name == null;
+//    }
 }
