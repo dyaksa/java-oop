8959654784ee35608938a0115019e215fc27224b
dyaksa <diasnour0395@gmail.com>
2024-04-16 10:15:13 +0700

    final class

---
+++ src/product/data/SocialMedia.java
@@ -0,0 +1,14 @@
+package product.data;
+
+public class SocialMedia {
+    public String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+// CANNOT INHERIT FINAL CLASS
+//class FakeFacebook extends Facebook {
+//
+//}
