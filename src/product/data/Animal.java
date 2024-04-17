c31c6240bd2b6a27436fb5902597c591d644a913
dyaksa <diasnour0395@gmail.com>
2024-04-17 10:20:54 +0700

    anotation

--- src/product/data/Animal.java
+++ src/product/data/Animal.java
@@ -1,5 +1,8 @@
 package product.data;
 
+import product.annotation.Fancy;
+
+@Fancy(name = "Animal", values = {"name", "run"})
 public abstract class Animal {
     public String name;
 
--- src/product/error/BlankException.java
+++ src/product/error/BlankException.java
@@ -1,7 +1,13 @@
 package product.error;
 
+@Deprecated
 public class BlankException extends RuntimeException {
     public BlankException(String message) {
         super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
     }
 }
