cbedcd04545a02b0423a1145a7391703dd21a073
dyaksa <diasnour0395@gmail.com>
2024-04-17 08:58:14 +0700

    exception

---
+++ src/product/application/ValidationApp.java
@@ -0,0 +1,20 @@
+package product.application;
+
+import product.data.LoginRequest;
+import product.error.ValidationException;
+import product.utils.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) {
+        LoginRequest loginRequest = new LoginRequest("", "rahasia");
+
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("data valid");
+        } catch (ValidationException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Selalu di eksekusi");
+        }
+    }
+}
---
+++ src/product/utils/ValidationUtil.java
@@ -0,0 +1,18 @@
+package product.utils;
+
+import product.data.LoginRequest;
+import product.error.ValidationException;
+
+public class ValidationUtil {
+    public static void validate(LoginRequest loginRequest) throws ValidationException, NullPointerException {
+        if (loginRequest.username() == null) {
+            throw new NullPointerException("username is null");
+        } else if (loginRequest.username().isBlank()) {
+            throw new ValidationException("username is blank");
+        } else if (loginRequest.password() == null) {
+            throw new NullPointerException("password is null");
+        } else if (loginRequest.password().isBlank()) {
+            throw new ValidationException("password is blank");
+        }
+    }
+}
